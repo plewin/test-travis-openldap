fc4f96c10ceb04a3b943c038b3e075756faedd21
Philippe Lewin <philippe.lewin@gmail.com>
2017-11-07 11:42:20 +0100

    Test ldap connection

--- tests/LdapClientTest.php
+++ tests/LdapClientTest.php
@@ -4,6 +4,20 @@ class LdapClientTest extends \PHPUnit_Framework_TestCase
 {
     public function testHello()
     {
-        $this->assertTrue(true);
+        $ldaprdn  = 'cn=Manager,dc=my-domain,dc=com';
+        $ldappass = 'secret';
+
+        $ldapconn = ldap_connect('localhost');
+        if(!$ldapconn) {
+            $this->fail("could not connect to ldap server");
+        }
+
+        $ldapbind = ldap_bind($ldapconn, $ldaprdn, $ldappass);
+
+        if (!$ldapbind) {
+            $this->assertTrue(true, 'Bind OK.');
+        } else {
+            $this->fail("could not bind to server");
+        }
     }
 }

f722694f30c4755109dff7b8751c3913f276a1fb
Philippe Lewin <philippe.lewin@gmail.com>
2017-11-07 11:45:42 +0100

    Oops, protocol error ^^"

--- tests/LdapClientTest.php
+++ tests/LdapClientTest.php
@@ -11,6 +11,7 @@ class LdapClientTest extends \PHPUnit_Framework_TestCase
         if(!$ldapconn) {
             $this->fail("could not connect to ldap server");
         }
+        ldap_set_option($ldapconn, LDAP_OPT_PROTOCOL_VERSION, 3);
 
         $ldapbind = ldap_bind($ldapconn, $ldaprdn, $ldappass);
 
